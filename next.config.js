d1544f3ea3f65ca514cb708f777b92c1d15eb1ec
Samuel Ko <koshunyin@gmail.com>
2022-10-17 13:27:04 +1100

    bug fix on configs

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	poweredByHeader: false,
-	eslint: {
-		dirs: ['src', 'tests'],
-	},
 	async rewrites() {
 		return [
 			{
