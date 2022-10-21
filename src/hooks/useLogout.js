6f33735cadd1a2c3feb61753b891aace8bcd8699
Samuel Ko <koshunyin@gmail.com>
2022-10-21 22:35:33 +1100

    bug fix on useLogout

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,11 +1,15 @@
+import React from 'react'
 import { useRouter } from 'next/router'
 import { useAjaxRequest } from './useAjaxRequest'
 import { HTTP_METHOD } from '../utils/constants'
+import { AuthContext } from '../components/organisms/AuthProvider'
 
 export const useLogout = (endpoint) => {
 	const [error, loading, sendRequest] = useAjaxRequest()
 
 	const router = useRouter()
+	const { mutate } = React.useContext(AuthContext)
+
 	const handleSubmit = async () => {
 		const request = {
 			url: endpoint,
@@ -13,7 +17,8 @@ export const useLogout = (endpoint) => {
 		}
 
 		await sendRequest(request, () => {
-			router.reload()
+			mutate()
+			router.push('/')
 		})
 	}
 
